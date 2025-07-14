ab7271438673ddd173b99a3f460af242d7380bb2
Rylie Weaver <rylie@MacBook-Pro.lan>
2025-07-13 23:24:49 -0400

    Change setup.py 3

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="wordle",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(include=["wordle", "wordle.*"]),
+    include_package_data=True,
+    package_data={
+        "wordle": ["**/*.json", "**/*.txt"],
+    },
     python_requires=">=3.9",
-    install_requires=[
-        "torch>=2.2",
-    ],
+    install_requires=["torch>=2.2"],
 )
